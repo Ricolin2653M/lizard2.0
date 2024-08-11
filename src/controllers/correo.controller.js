fcb4548159f84a9c9f73bb8930e286724022ead6
becariocarlos <becario.carlos@mideservices.com>
2024-08-11 15:54:15 -0600

    servicio para envio de correos

--- src/controllers/correo.controller.js
+++ src/controllers/correo.controller.js
@@ -1,31 +1,31 @@
 import nodemailer from 'nodemailer';
+import dotenv from 'dotenv';
+dotenv.config();
 
 export const sendEmail = async (req, res) => {
     const { remitente, asunto, mensaje } = req.body;
 
-    const pass = "tmbuvokzsamxzktw";
-    const gmail_user = "carlosbr0320@gmail.com";
- 
+    const mensajeNuevo = "Enviado por: " + remitente + "\n\n" + mensaje;
+
 
     // Configuración del transporte de nodemailer usando SMTP de Gmail
     const transporter = nodemailer.createTransport({
         service: 'gmail',
         auth: {
-            user: gmail_user, 
-            pass: pass
+            user: process.env.GMAIL_USER,
+            pass: process.env.PASS
         }
     });
 
     const mailOptions = {
-        remitente,
-        destinatario: gmail_user,
-        asunto,
-        mensaje
+        to: process.env.GMAIL_USER,
+        subject: asunto,
+        text: mensajeNuevo
     };
 
     try {
         await transporter.sendMail(mailOptions);
-        res.status(200).json({ message: 'Correo enviado exitosamente' });
+        res.status(200).json({ mailOptions });
     } catch (error) {
         console.error('Error al enviar el correo:', error);
         res.status(500).json({ message: 'Error al enviar el correo' });
